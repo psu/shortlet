57ca2b5d87fe81dcf9fab627c3c579246aea8055
psu <pontus@psu.se>
2022-10-14 13:50:35 +0000

    first try

---
+++ Chain.js
@@ -0,0 +1,17 @@
+const Chain = {
+  do: function (ms, fn, ... args) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(fn(...args))
+      }, ms)
+    })
+  },
+  now: function(fn, ... args) {
+    return (...args) => {
+      return new Promise((resolve, reject) => {
+        resolve(fn(...args))
+      })
+    }
+  }  
+}
+export default Chain
